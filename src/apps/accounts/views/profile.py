408324a8894694795ba5a197e4fa6d064f3536c0
joon0890 <kimjunhee2483@gmail.com>
2025-07-23 18:57:58 +0900

    refector: 프로젝트 전체적은 구조 수정

---
+++ src/apps/accounts/views/profile.py
@@ -0,0 +1,32 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from ..serializers import UserResponseSerializer
+from core.utils.responses import success_response, error_response
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_profile(request):
+    """
+    내 정보 조회 API
+    GET /api/users/me
+    """
+
+    try:
+        # 1. JWT에서 사용자 정보 가져오기
+        user = request.user
+
+        # 2. 시리얼라이저로 변환
+        serializer = UserResponseSerializer(user)
+
+        # 3. 성공 응답
+        return success_response(
+            data=serializer.data,
+            message="조회 성공"
+        )
+
+    except Exception as e:
+        return error_response(
+            error="사용자 정보 조회 중 오류가 발생했습니다.",
+            code=500
+        )
