f9dcc447303c399e4d822687cf3f7e2cf504b32c
joon0890 <kimjunhee2483@gmail.com>
2025-07-23 17:57:55 +0900

    refector: 에러 로직 추가, 분류기 입력 예외처리
    커스텀 에러 (이미지 분류 실패)

--- found_items/ml/predictor.py
+++ found_items/ml/predictor.py
@@ -1,8 +1,10 @@
 import os
 import torch
+from PIL import Image
+from pathlib import Path
 from .image_utils import download_image
 from .model_loader import ModelSingleton
-from pathlib import Path
+from .error import ImageClassificationError
 
 BASE_DIR = str(Path(__file__).resolve().parent.parent.parent)
 
@@ -11,19 +13,44 @@ DATA_DIR = os.path.join(BASE_DIR, 'lost_cls', 'data', 'train')
 CLASS_NAMES = sorted(os.listdir(DATA_DIR))
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def predict_image(image_url):    
+THRESHOLD = 0.7
+
+def predict_image(param):    
     try:
-        img = download_image(image_url)
+        # case 1: 이미지 링크 (str로 들어옴)
+        if isinstance(param, str) and param.startswith("http"):
+            img = download_image(param)
+
+        # case 2: 파일 객체 (예: request.FILES['image'])
+        elif hasattr(param, "read"):
+            img = Image.open(param).convert("RGB")
+
+        else:
+            raise ValueError("지원하지 않는 이미지 입력 형식입니다.")
+        
         img_t = ModelSingleton.get_transform()(img).unsqueeze(0).to(DEVICE)
         model = ModelSingleton.get_model()
         with torch.no_grad():
             output = model(img_t)
             prob = torch.nn.functional.softmax(output, dim=1)
-            print(prob)
+
             pred_idx = prob.argmax().item()
-            pred_label = CLASS_NAMES[pred_idx]
             confidence = prob[0, pred_idx].item()
-        return pred_label  # 카테고리만 반환
+            pred_label = CLASS_NAMES[pred_idx]
+
+            if confidence >= THRESHOLD:
+                return {
+                    "label": pred_label,
+                    "confidence": round(confidence, 4),
+                    "status": "certain"
+                }
+            else:
+                return {
+                    "label": "unknown",
+                    "confidence": round(confidence, 4),
+                    "status": "uncertain"
+                }
+            
     except Exception as e:
-        print(f"[Error] 이미지 분류 실패: {e}")
-        return None  # 분류 실패 시 None 또는 'unknown' 등
+        raise ImageClassificationError(f"이미지 분류 실패: {e}")
+    
