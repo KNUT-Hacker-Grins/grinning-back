eaf6ab7b6e7914ea9e624c0c0ab460c617897ed1
joon0890 <kimjunhee2483@gmail.com>
2025-08-14 15:27:27 +0900

    fix: resnet에서 yolo로 변경

--- src/classifier/image_utils.py
+++
@@ -1,8 +0,0 @@
-import requests
-from PIL import Image
-from io import BytesIO
-
-def download_image(image_url):
-    response = requests.get(image_url, timeout=5)
-    response.raise_for_status()
-    return Image.open(BytesIO(response.content)).convert('RGB')
