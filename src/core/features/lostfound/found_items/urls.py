59e78d8a75c32ff27c91b821809fe378be933153
joon0890 <kimjunhee2483@gmail.com>
2025-08-18 15:50:20 +0900

    엔드포인트 수정

--- src/core/features/lostfound/found_items/urls.py
+++ src/core/features/lostfound/found_items/urls.py
@@ -8,9 +8,9 @@ from .views import (
 urlpatterns = [
     path('', FoundItemCreateView.as_view(), name='founditem_create'),
     path('list', FoundItemListView.as_view(), name='founditem_list'),
+    path('list/admin', AdminFoundItemListView.as_view(), name='admin_founditem_list'),
     path('<int:id>/', FoundItemDetailView.as_view(), name='founditem_detail'),
     path('<int:id>/edit', FoundItemUpdateView.as_view(), name='founditem_update'),
     path('<int:id>/delete', FoundItemDeleteView.as_view(), name='founditem_delete'),
     path('<int:id>/status', FoundItemStatusUpdateView.as_view(), name='founditem_status_update'),
-    path('admin', AdminFoundItemListView.as_view(), name='admin_founditem_list')
 ]
