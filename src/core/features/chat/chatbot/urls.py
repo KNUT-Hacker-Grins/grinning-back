97c516043ed34834721eec4ad8e762f5e8bf0494
joon0890 <kimjunhee2483@gmail.com>
2025-08-21 11:05:40 +0900

    엔드포인트 수정

--- src/core/features/chat/chatbot/urls.py
+++ src/core/features/chat/chatbot/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ChatbotMessageView, ChatbotHealthView
 
 urlpatterns = [
-    path('message', ChatbotMessageView.as_view(), name='chatbot_message'),
+    path('session/start', ChatbotMessageView.as_view(), name='chatbot_message'),
     path('health', ChatbotHealthView.as_view(), name='chatbot_health'),
 ]
