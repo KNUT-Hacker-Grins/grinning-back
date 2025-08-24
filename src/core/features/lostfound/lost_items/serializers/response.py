945da62690b2218502da05142cafa42440f9261b
kth4778 <cnblue123429@gmail.com>
2025-08-24 10:13:50 +0900

    feat: lost 게시물

--- src/core/features/lostfound/lost_items/serializers/response.py
+++ src/core/features/lostfound/lost_items/serializers/response.py
@@ -9,7 +9,8 @@ User = get_user_model() # Get the User model
 class LostItemResponseSerializer(serializers.ModelSerializer):
     """분실물 신고 응답용 시리얼라이저"""
 
-    user_profile_picture_url = serializers.SerializerMethodField() # Add this line
+    user_profile_picture_url = serializers.SerializerMethodField()
+    user_name = serializers.SerializerMethodField() # Added this line
 
     class Meta:
         model = LostItem
@@ -24,20 +25,24 @@ class LostItemResponseSerializer(serializers.ModelSerializer):
             'category',
             'reward',
             'status',
-            'user_profile_picture_url', # Add this line
+            'user_profile_picture_url',
+            'user_name', # Added this line
         ]
-        read_only_fields = ['id', 'status', 'lost_at', 'lost_location', 'latitude', 'longitude', 'user_profile_picture_url']
+        read_only_fields = ['id', 'status', 'lost_at', 'lost_location', 'latitude', 'longitude', 'user_profile_picture_url', 'user_name'] # Added user_name
 
-    def get_user_profile_picture_url(self, obj): # Add this method
+    def get_user_profile_picture_url(self, obj):
         if obj.user and obj.user.profile_picture_url:
             return obj.user.profile_picture_url
         return None # Or a default placeholder URL
 
+    def get_user_name(self, obj): # Added this method
+        if obj.user and hasattr(obj.user, 'name'):
+            return obj.user.name
+        return "익명" # Default to "익명" if user or user.name is not available
+
     def to_representation(self, instance):
-        # Get the default representation
         ret = super().to_representation(instance)
         
-        # Ensure image_urls is always a list
         if not isinstance(ret.get('image_urls'), list):
             ret['image_urls'] = []
         
