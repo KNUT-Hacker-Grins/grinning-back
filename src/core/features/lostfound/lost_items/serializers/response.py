ddffeb5850a790e220dbcc2563db2b180093bd2d
kth4778 <cnblue123429@gmail.com>
2025-08-25 12:17:47 +0900

    feat: lost 상세 게시물 디버그

--- src/core/features/lostfound/lost_items/serializers/response.py
+++ src/core/features/lostfound/lost_items/serializers/response.py
@@ -6,16 +6,21 @@ from django.contrib.auth import get_user_model # Import get_user_model
 User = get_user_model() # Get the User model
 
 
-class LostItemResponseSerializer(serializers.ModelSerializer):
-    """분실물 신고 응답용 시리얼라이저"""
+class OwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'name', 'profile_picture_url']
 
-    user_profile_picture_url = serializers.SerializerMethodField()
-    user_name = serializers.SerializerMethodField()
+
+class LostItemResponseSerializer(serializers.ModelSerializer):
+    """분실물 신고 응답용 시리얼라이저 (Refactored)"""
+    user = OwnerSerializer(read_only=True)
 
     class Meta:
         model = LostItem
         fields = [
             'id',
+            'user', # Use the nested OwnerSerializer
             'title',
             'description',
             'lost_at',
@@ -26,25 +31,25 @@ class LostItemResponseSerializer(serializers.ModelSerializer):
             'category',
             'reward',
             'status',
-            'user_profile_picture_url',
-            'user_name',
         ]
-        read_only_fields = ['id', 'status', 'lost_at', 'lost_location', 'latitude', 'longitude', 'user_profile_picture_url', 'user_name']
-
-    def get_user_profile_picture_url(self, obj):
-        if obj.user and obj.user.profile_picture_url:
-            return obj.user.profile_picture_url
-        return "/default-profile.png"
-
-    def get_user_name(self, obj):
-        if obj.user and hasattr(obj.user, 'name'):
-            return obj.user.name
-        return "익명"
+        read_only_fields = fields # Make all fields read-only for this response serializer
 
     def to_representation(self, instance):
         ret = super().to_representation(instance)
         
+        # Ensure image_urls is always a list
         if not isinstance(ret.get('image_urls'), list):
             ret['image_urls'] = []
         
+        # Handle case where user might be null
+        if not ret.get('user'):
+            ret['user'] = {
+                'id': None,
+                'email': '익명',
+                'name': '익명',
+                'profile_picture_url': '/default-profile.png'
+            }
+        elif not ret.get('user').get('profile_picture_url'):
+            ret['user']['profile_picture_url'] = '/default-profile.png'
+
         return ret
