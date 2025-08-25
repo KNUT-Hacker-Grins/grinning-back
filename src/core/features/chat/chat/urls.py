fa331e188b3f27a4699b750652960a8c53018c95
kth4778 <cnblue123429@gmail.com>
2025-08-25 18:37:34 +0900

    feat: 채팅창 디버그

--- src/core/features/chat/chat/urls.py
+++ src/core/features/chat/chat/urls.py
@@ -11,7 +11,7 @@ from .views import (
 urlpatterns = [
     path('start', StartChatView.as_view(), name='start_chat'),
     path('rooms', ChatRoomListView.as_view(), name='chat-room-list'),
-    path('<int:room_id>/list', ChatMessageListView.as_view(), name='chat_message_list'),
+    path('<int:room_id>/messages', ChatMessageListView.as_view(), name='chat_message_list'),
     path('<int:room_id>/message', ChatMessageCreateView.as_view(), name='chat_message_create'),
     path('unread-count/', UnreadCountView.as_view(), name='unread-count'),
     path('mark-as-read/<int:room_id>/', MarkAsReadView.as_view(), name='mark-as-read'),
